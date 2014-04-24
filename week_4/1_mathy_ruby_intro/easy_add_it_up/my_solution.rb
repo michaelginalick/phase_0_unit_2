4c6f5bdedf591354afe7089c99c3b0db9180151d
michaelginalick <michael.ginalick@gmail.com>
2014-04-23 23:24:59 -0500

    Update my_solution.rb

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -20,6 +20,61 @@
 
 # 3. Refactored Solution
 
+require_relative "my_solution"
+
+
+def total(array)
+  sum = 0
+  array.inject{|sum,x| sum + x}
+def sentence_maker(words)
+    words.map! { |x| x.class == String ? x : x.to_s }
+    words.map.downcase!
+    words[0].capitalize!
+    words[-1] = "#{words[-1]}."
+    words.join(" ")
+end
+
+
+describe 'total' do
+  let(:array_1) { [1, 2, 3, 4, 5, 5, 7] }
+  let(:array_2) { [4, 4, 5, 5, 6, 6, 6, 7] }
+
+  it "is defined as a method" do
+    defined?(total).should eq 'method'
+  end
+
+  it "requires a single argument" do
+    method(:total).arity.should eq 1
+  end
+
+  it "returns the correct total of array 1" do
+    total(array_1).should eq 27
+  end
+
+  it "returns the correct total of array 2" do
+    total(array_2).should eq 43
+  end
+end
+
+describe 'sentence_maker' do
+  let(:first_sentence) {["all", "my", "socks", "are", "dirty"]}
+  let(:second_sentence) {["alaska", "has", "over", 586, "thousand", "miles"]}
+
+  it "is defined as a method" do
+    defined?(sentence_maker).should eq 'method'
+  end
+
+  it "requires a single argument" do
+    method(:sentence_maker).arity.should eq 1
+  end
+
+  it "returns the grammatically correct sentence of the first sentence" do
+    sentence_maker(first_sentence).should eq "All my socks are dirty."
+  end
+
+
+
+
 
 
 # 4. Reflection 
