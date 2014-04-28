9a2ac73fc89e4742ce68833d182ff8b9964676ba
michaelginalick <michael.ginalick@gmail.com>
2014-04-28 15:10:05 -0500

    Update my_solution.rb

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -12,30 +12,56 @@
 
 # 3. Initial Solution
 
-class Die
-  def initialize(sides)
+#class Die
+ # def initialize(sides)
     # code goes here
-  end
+  #end
   
-  def sides
+  #def sides
     # code goes here
-  end
+  #end
   
-  def roll
+  #def roll
     # code goes here
-  end
-end
+  #end
+#end
 
 
 
 # 4. Refactored Solution
 
 
+class Die
+  def initialize(sides)
+  	@sides = sides
+  	unless sides >= 1
+  		raise ArgumentError.new ("Only numbers 1 and greater are allowed")
+  	end
+
+
+  end
+  
+  def sides
+  	@sides
+  end
+ 
+  def roll
+    return 1 + rand(@sides)
+  end
+end
+
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+
+createDie = Die.new(6)
+
+p createDie.sides == 6
+p createDie.roll >= 1
+p createDie.roll <= 6
+
 
 
 
