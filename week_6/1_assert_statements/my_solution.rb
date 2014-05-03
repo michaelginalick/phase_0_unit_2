9e61f52ce461225c08648c42ee5b94b210ec6ad5
michaelginalick <michael.ginalick@gmail.com>
2014-05-03 18:55:58 -0500

    Update my_solution.rb

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,32 +1,132 @@
-# U2.W6: Testing Assert Statements
+#!/usr/local/bin/ruby -w
+
+# U2.W6: Drawer Debugger
+
 
 # I worked on this challenge [by myself, with: ].
 
 
-# 2. Review the simple assert statement
+# 2. Original Code
 
-def assert
-  raise "Assertion failed!" unless yield
+class Drawer
+
+attr_reader :contents
+
+# Are there any more methods needed in this class?
+
+def initialize
+@contents = []
+@open = true
 end
 
-name = "bettysue"
-assert { name == "bettysue" }
-assert { name == "billybob" }
+def open
+@open = true
+puts "Your drawer is open."
+end
 
-# 2. Pseudocode what happens when the code above runs
- #the method assert is defined but accepts no arguements. The logic says fail unless yield.
- # name is declared as bettysue and we are passing the method assert name two seperate values.
- # bettysue passes because name = bettysue, billybob failes because it does not
+def close
+@open = false
+puts "Your drawer is closed."
+end 
 
-# 3. Copy your selected challenge here
+def add_item(item)
+@contents << item
+puts "A #{item} was added to the drawer."
+end
+
+def remove_item(item = @contents.pop) #what is `#pop` doing? # pop is returning the last item in contents array
+@contents.delete(item)
+end
+
+def dump  # what should this method return? This should return true if contents array is empty
+@contents.clear
+puts "Your drawer is empty."
+end
+
+def view_contents
+puts "The drawer contains:"
+@contents.each {|silverware| puts "- " + silverware.type }
+end
+end
+
+class Silverware
+attr_reader :type
+
+# Are there any more methods needed in this class?
+
+def initialize(type, clean = true)
+@type = type
+@clean = clean
+end
+
+def eat
+puts "eating with the #{type}"
+@clean = false
+end
+
+def clean_silverware
+ puts "cleaned #{type}"
+ @clean = true
+end
+
+
+def clean
+  puts "#{type} is clean"
+  @clean = true
+end
+
+
+end
+
+knife1 = Silverware.new("knife")
+
+silverware_drawer = Drawer.new
+silverware_drawer.add_item(knife1) 
+silverware_drawer.add_item(Silverware.new("spoon"))
+silverware_drawer.add_item(Silverware.new("fork")) 
+silverware_drawer.view_contents
+
+silverware_drawer.remove_item
+silverware_drawer.view_contents
+sharp_knife = Silverware.new("sharp_knife")
+
+
+silverware_drawer.add_item(sharp_knife)
+
+silverware_drawer.view_contents
+
+removed_knife = silverware_drawer.remove_item(sharp_knife)
+removed_knife.eat
+removed_knife.clean_silverware 
+
+silverware_drawer.view_contents
+silverware_drawer.dump
+silverware_drawer.view_contents #What should this return? The contents of the drawer
+
+
+fork = Silverware.new("fork")
+silverware_drawer.add_item(fork)
+
+fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+fork.eat
+
+#BONUS SECTION
+puts fork.clean
+
+# DRIVER TESTS GO BELOW THIS LINE
+
+silverware_drawer.open
+silverware_drawer.close
+silverware_drawer.add_item(chopsticks = Silverware.new("ladel"))
+silverware_drawer.view_contents
+chopsticks.eat == "eating with the ladel"
+chopsticks.clean
 
 
 
 
-# 4. Convert your driver test code from that challenge into Assert Statements
+# 5. Reflection 
 
 
-
-
-
-# 5. Reflection
+# this was a really great excerise. it was very helpful to be able to see how the different methods work together to perform 
+#
