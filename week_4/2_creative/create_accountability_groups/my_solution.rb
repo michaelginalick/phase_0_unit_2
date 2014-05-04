08aac1e094c37560f136a42db6c251150cb3a5a5
michaelginalick <michael.ginalick@gmail.com>
2014-05-03 23:27:01 -0500

    Update my_solution.rb

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -12,8 +12,24 @@
 
 # 3. Initial Solution
 
-
-
+require 'set'
+people = ["Ahmed Al-Bahar", "Ann Lin", "Brendan Brown", "Caleb Rugg", 
+  "Carl Krause", "Catherine Farkas", "Daniel Yoachim", "Keenan Turner", 
+	"Kim Girard", "Lana Rakhman", "Mike Daugherty", "Mikee Pourhadi", 
+	"Natalie Frecka", "Natalie McCroy", "Peter Debelak", "Rick Dsida", 
+	"Scott Silveus", "Travis Hernandez", "Tyler Keating", "Xander Glassman", 
+	"Zac Mitton"] 
+	
+	def group_gen(Array)
+	    	random = array.shuffle
+	      group = Array.new
+	       i = 0
+	      while random.length > 3
+		      group[i]= random.take(4)
+		      random = random.drop(4)
+		      i += 1
+	end
+end
 
 # 4. Refactored Solution
 
