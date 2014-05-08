670ff0c0028e0e806cdb4cb9ac9d430ea92f7a2e
michaelginalick <michael.ginalick@gmail.com>
2014-05-07 19:54:56 -0500

    Update my_solution.rb

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,109 +1,78 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge with Scott Silveus.
-# Note: Even though this is a solo challenge, Scott and I both did the PezDispenser
-# challenge solo, so we took the opportunity to work through some difficulties
-# together on this challenge.
+# I worked on this challenge [by myself, with: ].
 
 
 # Pseudocode
-# CREATE class Song
-# 	make title and artist variables readable
 
-# 	DEFINE initialize method with two arguments, title and artist
-# 		SET both inputs to instance variables with the same names
-# 	END initialize method
 
-# 	DEFINE play method (no arguments)
-# 		RETURN self
-# 	END play method
-
-# END class Song
-
-# CREATE class Playlist
-
-# 	DEFINE initialize method with unknown number of arguments, songs
-# 		SET arguments to instance variable called playlist
-# 	END initialize
-
-# 	DEFINE add method with unknown number of arguments, new_songs
-# 		for EACH song in new_songs
-#			add song to playlist
-#		END each
-# 	END add method
-
-# 	DEFINE num_of_tracks method (no arguments)
-# 		RETURN the length of playlist array
-# 	END num_of_tracks method
-
-# 	DEFINE remove method with unknown number of arguments, songs_to_delete
-# 		for EACH element in songs_to_delete
-# 			DELETE the element from playlist
-# 		END each
-# 	END remove method
-
-# 	DEFINE includes? method that takes 1 argument, song
-# 		return true if song is in playlist, false otherwise
-# 	END includes?
-
-# 	DEFINE play_all method (no arguments)
-# 		RETURN a list of all the songs in the playlist
-# 	END play_all
-
-# 	DEFINE display method (no arguments)
-# 		puts all the titles, artists of the songs in playlist
-# 	END display
-	
-# END class Playlist
 
 # Initial Solution
-class Song
-	attr_reader :title, :artist 
 
-	def initialize(title, artist)
-		@title = title
-		@artist = artist
-	end
 
-	def play
-		return self
-	end
-end
-
-class Playlist
-	def initialize(*songs)
-		@playlist = songs
-	end
-
-	def add(*new_songs)
-		new_songs.each {|song| @playlist << song}
-	end
-
-	def num_of_tracks
-		@playlist.length
-	end
-
-	def remove(*songs_to_delete)
-		songs_to_delete.each {|song| @playlist.delete(song)}
-	end
-
-	def includes?(song)
-		@playlist.include?(song)
-	end
-
-	def play_all
-		@playlist.each {|song| song.play}
-	end
-
-	def display
-		@playlist.each{|song| puts "#{song.title}, #{song.artist}"}
-	end
-end
 
 
 # Refactored Solution
 
+
+#def view_contents
+# puts "The song contains:"
+      # @song.each {|my_playlist| puts "-" 
+
+      class Song
+      	attr_reader :title, :artist 
+
+      	def initialize(title, artist)
+      		@title = title
+      		@artist = artist
+      	end
+      	
+      	def play(*song_playing)
+      	   song_playing.each {|song| @song.play(song)}
+    	  end
+    	  
+      end
+
+
+
+class Playlist
+  attr_reader :song
+  
+    def initialize(*songs)
+    @playlist = songs
+   end
+  
+  def add(*new_tracks)
+    new_tracks.each{|song| @playlist << song}
+    
+  end
+  
+  def num_of_tracks
+    puts @playlist.length
+  end
+  
+  def remove(*remove_song_from_playlist)
+    remove_song_from_playlist.each {|song| @playlist.delete(song)}
+  end
+  
+  def includes?(song)
+    @playlist.include?(song)
+  end
+  
+  def play_all
+    @playlist.each{|song| song.play}
+  end
+  
+  def display
+    @playlist.each{|song| puts "#{song.title}, #{song.artist}"}
+  end
+  
+  #def display
+  #puts @playlist.to_s
+  #end
+  
+end
 
 
 
@@ -129,4 +98,7 @@ my_playlist.display
 
 
 
+
+
 # Reflection 
+#I liked the concept of this challange. I felt it was difficult and it took me awhile, but the answers made sense to me.
