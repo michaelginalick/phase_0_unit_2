9e9bc418ee2034e392dccb3b0cb8930d80ede25d
Michael <Michael.ginalick@gmail.com>
2014-06-07 19:00:43 -0500

    gps2.1

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -26,18 +26,18 @@ def bakery_num(num_of_people, fav_food)
     while num_of_people > 0
     
     
-      if num_of_people / my_list["pie"] > 0 && fav_food == "pie" 
-        pie_qty = num_of_people / my_list["pie"]    
-        num_of_people = num_of_people % my_list["pie"]
+      if num_of_people / my_list["pie"] > 0 && fav_food == "pie"  #41/8
+         pie_qty = num_of_people / my_list["pie"]    #=5.1 
+         num_of_people = num_of_people % my_list["pie"] # 1
     
-      elsif num_of_people / my_list["cake"] > 0 && fav_food == "cake"
-        cake_qty = num_of_people / my_list["cake"]
-        num_of_people = num_of_people % my_list["cake"]
+      elsif  num_of_people / my_list["cake"] > 0 && fav_food == "cake"
+             cake_qty = num_of_people / my_list["cake"]
+             num_of_people = num_of_people % my_list["cake"]
         
         # 
         
       else
-        cookie_qty = num_of_people
+        cookie_qty = num_of_people #1
         num_of_people = 0
       end
     end
@@ -52,7 +52,7 @@ def bakery_num(num_of_people, fav_food)
 # These are the tests to ensure it's working. 
 # These should all print true if the method is working properly.
 
-p bakery_num(24, "cake") #== "You need to make 4 cake(s)." # works
+p bakery_num(24, "cake") == "You need to make 4 cake(s)." # works
 p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
 p bakery_num(24, "cookie") == "You need to make 24 cookie(s)."
 p bakery_num(4, "pie") == "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
@@ -60,6 +60,6 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") #== "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+p bakery_num(41, "cake") == "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
  
  
